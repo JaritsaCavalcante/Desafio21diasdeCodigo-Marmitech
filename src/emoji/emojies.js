64eecd02f0272a8a817690938a74ef8eed3ff3c0
mariajazinha <1turmashow@gmail.com>
2022-09-04 21:52:56 -0300

    emoji

---
+++ src/emoji/emojies.js
@@ -0,0 +1,45 @@
+import { getEmojies } from './api.js'
+
+const emojieDiv = document.querySelector('#emojieDiv')
+
+export async function render() {
+  const emojies = await getEmojies()
+  // console.log(emojies)
+
+  emojieDiv.innerHTML = emojies
+    ?.map(emojie => renderEmojieCard(emojie))
+    .join('')
+
+  // Function that copy the emojie
+  // PS: I couldn't module it
+  let element = document.querySelectorAll('.emoji-card span')
+  // let tooltip = document.querySelector('.tooltip-text')
+  // console.log(element)
+  element.forEach(item => {
+    item.addEventListener('click', () => {
+      let emojieCopy = item.innerText
+      navigator.clipboard.writeText(emojieCopy)
+      console.log(emojieCopy)
+      showTooltip(emojieCopy)
+    })
+  })
+}
+
+// Copy to clipboard function
+function showTooltip(emojieCopy) {
+  Toastify({
+    text: `${emojieCopy} Copiado para a área de transferência`,
+    position: 'center',
+    duration: 2000
+  }).showToast()
+}
+
+export function renderEmojieCard(emojie) {
+  return `
+    <div class="emoji-card">
+      <span class="tooltip-text">Copied !</span>
+      <span class="emoji-char">${emojie.character}</span>
+      <p class="unicode-name">${emojie.unicodeName}</p>
+    </div>
+  `
+}
